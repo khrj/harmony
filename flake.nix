475bcbab6d14c2877e52174701fa894820926703
Khushraj Rathod <khushraj.rathod@gmail.com>
2024-06-22 17:19:15 +0530

    overhaul code style

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+		flake-utils.url = "github:numtide/flake-utils";
+	};
+
+	outputs = { self, nixpkgs, flake-utils }:
+		flake-utils.lib.eachDefaultSystem (system:
+			let pkgs = import nixpkgs { inherit system; config = { allowUnfree = true; }; };
+			in {
+				devShells.default = pkgs.mkShell {
+					buildInputs = with pkgs; [
+						nodejs_20
+						yt-dlp
+						google-chrome
+					];
+					
+					shellHook = ''
+						export PUPPETEER_EXECUTABLE_PATH="${pkgs.google-chrome}/bin/google-chrome-stable"
+						export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=1
+    				'';
+				};
+			}
+		);
+}
--- index.js
+++ index.js
@@ -1,241 +1,259 @@
 #!/usr/bin/env node
-const {program} = require('commander')
-program.version('1.0.0')
+import { program } from "commander"
+program.version("1.0.0")
 
-const {yellow} = require('chalk')
-const spinner = require('ora')()
-const puppeteer = require('puppeteer')
-const fs = require('fs').promises;
-const { join } = require('path')
-const url = require('url')
+import chalk from "chalk"
+import clear from "clear"
+import figlet from "figlet"
+import { promises as fs } from "fs"
+import ora from "ora"
+import { join } from "path"
+import { launch } from "puppeteer"
+
+const spinner = ora()
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 program
-    .option('-v, --verbose', 'Output debug info')
-    .option('-l, --headfull', 'Run chromium headfull instead of headless')
-    .requiredOption('-u, --url <Your meeting URL>', 'URL of meeting to join')
-    .requiredOption('-e, --email <Your email address>', 'Google account email')
-    .requiredOption('-p, --password <Your email address password>', 'Google account password')
+	.option("-v, --verbose", "Output debug info")
+	.option("-l, --headfull", "Run chromium headfull instead of headless")
+	.requiredOption("-u, --url <Your meeting URL>", "URL of meeting to join")
+	.requiredOption("-e, --email <Your email address>", "Google account email")
+	.requiredOption("-p, --password <Your email address password>", "Google account password")
 
-program.parse(process.argv)
-require('clear')()
+program.parse()
+clear()
 
-if (program.verbose) console.log(program.opts())
+const options = program.opts()
 
-console.log(yellow(require("figlet").textSync('Meetusic Bot', {
-    font: 'Standard',
-    horizontalLayout: 'fitted',
-})))
+if (options.verbose) console.log(options)
 
-;(async () => {
-    spinner.start("Opening browser")
-    const browser = await puppeteer.launch(
-        {
-            args: [
-                '--use-fake-ui-for-media-stream',
-                '--mute-audio'
-            ],
-            headless: !program.headfull,
-            defaultViewport: null
-        })
-    const page = (await browser.pages())[0]
-    await page.setExtraHTTPHeaders({'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8'})
-    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36')
-    spinner.succeed("Opened browser")
-    spinner.start("Loading cookies (Google account)")
-    try {
-        const cookies = JSON.parse(await fs.readFile('./cookies.json'));
-        await page.setCookie(...cookies);
-        spinner.succeed("Loaded cookies (Google account)")
-    } catch (e) {
-        spinner.warn("No cookies yet")
-    }
+console.log(
+	chalk.yellow(
+		figlet.textSync("Meetusic Bot", {
+			font: "Standard",
+			horizontalLayout: "fitted",
+		})
+	)
+)
 
-    let nav
-    spinner.start("Loading page")
-    await page.goto("https://meet.google.com/", {waitUntil: "load"})
-    let domain = url.parse(page.url()).host
+spinner.start("Opening browser")
 
-    if (domain === "apps.google.com") {
-        spinner.succeed("Loaded page")
-        spinner.info("Not signed in")
-        spinner.start("Signing in")
-        await page.goto("https://accounts.google.com/", {waitUntil: "networkidle0"})
-        await page.focus('#identifierId')
-        await page.keyboard.type(program.email)
-        nav = page.waitForNavigation({waitUntil: "networkidle0"})
-        await page.keyboard.press('Enter')
-        await nav
-        await page.waitFor(2000)
-        await page.focus('#identifierId')
-        await page.keyboard.type(program.password)
-        nav = page.waitForNavigation({waitUntil: "networkidle0"})
-        await page.keyboard.press('Enter')
-        await nav
+const browser = await launch({
+	args: ["--use-fake-ui-for-media-stream", "--mute-audio"],
+	headless: !options.headfull,
+	defaultViewport: null,
+})
 
-        if (await page.$eval('title', e => e.innerHTML) === "Google Account") {
-            spinner.succeed("Signed in successfully")
-            spinner.start("Writing session cookies as './cookies.json'")
-            const cookies = await page.cookies();
-            await fs.writeFile(join('.', 'cookies.json'), JSON.stringify(cookies, null, 2));
-            spinner.succeed("Wrote session cookies as './cookies.json'")
-        } else {
-            spinner.fail("Sign in error")
-            process.exit(1)
-        }
-    } else if (domain === "meet.google.com") {
-        spinner.succeed("Loaded page")
-        spinner.info("Already signed in")
-    } else {
-        spinner.fail(`Unknown domain: ${domain}`)
-    }
+const page = (await browser.pages())[0]
+await page.setExtraHTTPHeaders({ "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8" })
+await page.setUserAgent(
+	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36"
+)
 
-    spinner.start("Joining meeting")
-    nav = page.waitForNavigation({waitUntil: 'networkidle0'})
+spinner.succeed("Opened browser")
+spinner.start("Loading cookies (Google account)")
 
-    await page.goto(program.url, {waitUntil: "domcontentloaded"})
-    await page.evaluate(() => {
-        let stream = new Promise(resolve => {
-            let audio = document.createElement("audio")
-            audio.setAttribute("src", "https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3")
-            audio.setAttribute("crossorigin", "anonymous")
-            audio.setAttribute("controls", "")
-            audio.setAttribute("loop", "")
-            audio.onplay = _ => {
-                let stream = audio.captureStream()
-                console.log(stream.getAudioTracks())
-                resolve(stream)
-            }
-            document.querySelector("body").appendChild(audio)
-            audio.play()
-        })
-        navigator.mediaDevices.getUserMedia = async _ => {
-            return stream
-        }
-    })
+try {
+	const cookies = JSON.parse(await fs.readFile("./cookies.json"))
+	await page.setCookie(...cookies)
+	spinner.succeed("Loaded cookies (Google account)")
+} catch (e) {
+	spinner.warn("No cookies yet")
+}
 
-    await nav
+let nav
+spinner.start("Loading page")
+// await page.goto("https://meet.google.com/", { waitUntil: "load" })
 
-    // Join meeting
-    await page.evaluate(_ => {
-        let spans = document.getElementsByTagName("span")
-        for (const span of spans) {
-            if (span.innerHTML === "Join now") {
-                span.click()
-            } else if (span.innerHTML === "Ask to join") {
-                span.click()
-            }
-        }
-    })
 
-    await page.waitFor('[data-tooltip="Chat with everyone"]', {timeout: 0})
-    spinner.succeed("Joined meeting")
+await page.goto(options.url, { waitUntil: "domcontentloaded" })
+let domain = new URL(page.url()).host
 
-    spinner.start("Sending intro message")
-    await page.click('[data-tooltip="Chat with everyone"]')
-    await page.waitFor(1000) // 300 ms transition + 700 buffer
-    await page.click('[name="chatTextInput"]')
+await sleep(100000)
 
-    const shiftEnter = async _ => {
-        await page.keyboard.down('Shift')
-        await page.keyboard.press('Enter')
-        await page.keyboard.up('Shift')
-    }
+if (domain === "workspace.google.com") {
+	spinner.succeed("Loaded page")
+	spinner.info("Not signed in")
+	spinner.start("Signing in")
 
-    await page.keyboard.type("Hello! I'm Meetusic Bot, a bot that helps you play music from online sources. type \"@meetusicbot /help\" (without quotes) to see what I can do")
-    await shiftEnter()
-    await page.keyboard.type("Version: 1")
-    await shiftEnter()
-    await page.keyboard.type("Developed by @khrj")
-    await page.click('[data-tooltip="Send message"]')
-    spinner.succeed("Send intro message")
+	await page.goto("https://accounts.google.com/", { waitUntil: "networkidle0" })
+	await page.focus("#identifierId")
+	await page.keyboard.type(options.email)
+	nav = page.waitForNavigation({ waitUntil: "networkidle0" })
+	await page.keyboard.press("Enter")
+	await nav
+	await page.waitFor(2000)
+	await page.focus("#identifierId")
+	await page.keyboard.type(options.password)
+	nav = page.waitForNavigation({ waitUntil: "networkidle0" })
+	await page.keyboard.press("Enter")
+	await nav
 
-    // Get messages from window
-    await page.exposeFunction('message', async msg => {
-        if (msg.text.includes("@meetusicbot") && msg.sender !== 'You') {
-            msg.text = msg.text.replace('@meetusicbot', '')
+	if ((await page.$eval("title", e => e.innerHTML)) === "Google Account") {
+		spinner.succeed("Signed in successfully")
+		spinner.start("Writing session cookies as './cookies.json'")
+		const cookies = await page.cookies()
+		await fs.writeFile(join(".", "cookies.json"), JSON.stringify(cookies, null, 2))
+		spinner.succeed("Wrote session cookies as './cookies.json'")
+	} else {
+		spinner.fail("Sign in error")
+		process.exit(1)
+	}
+} else if (domain === "meet.google.com") {
+	spinner.succeed("Loaded page")
+	spinner.info("Already signed in")
+} else {
+	spinner.fail(`Unknown domain: ${domain}`)
+}
 
-            let parsed = {
-                command: false,
-                args: false,
-                error: false
-            }
+spinner.start("Joining meeting")
+nav = page.waitForNavigation({ waitUntil: "networkidle0" })
 
-            // Find command
-            for (const word of msg.text.trim().split(' ')) {
-                if (word.startsWith('/')) {
-                    if (!parsed.command) {
-                        parsed.command = word.replace('/', '')
-                    } else {
-                        parsed.error = "Too many commands"
-                        parsed.command = false
-                        parsed.args = false
-                        break
-                    }
-                }
-            }
+await page.goto(options.url, { waitUntil: "domcontentloaded" })
+await page.evaluate(() => {
+	let stream = new Promise(resolve => {
+		let audio = document.createElement("audio")
+		audio.setAttribute("src", "https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3")
+		audio.setAttribute("crossorigin", "anonymous")
+		audio.setAttribute("controls", "")
+		audio.setAttribute("loop", "")
+		audio.onplay = _ => {
+			let stream = audio.captureStream()
+			console.log(stream.getAudioTracks())
+			resolve(stream)
+		}
+		document.querySelector("body").appendChild(audio)
+		audio.play()
+	})
+	navigator.mediaDevices.getUserMedia = async _ => {
+		return stream
+	}
+})
 
-            if (!parsed.error) {
-                if (!parsed.command) {
-                    parsed.error = "No commands"
-                } else {
-                    parsed.args = msg.text.replace('/' + parsed.command, '').trim()
-                }
-            }
+await nav
 
-            console.log(parsed)
+// Join meeting
+await page.evaluate(_ => {
+	let spans = document.getElementsByTagName("span")
+	for (const span of spans) {
+		if (span.innerHTML === "Join now") {
+			span.click()
+		} else if (span.innerHTML === "Ask to join") {
+			span.click()
+		}
+	}
+})
 
-            // Continue logic here
-            if (parsed.error) {
-                await page.keyboard.type(`Error: ${parsed.error}`)
-                await page.click('[data-tooltip="Send message"]')
-            } else {
-                //TODO
-            }
-        }
-    })
+await page.waitFor('[data-tooltip="Chat with everyone"]', { timeout: 0 })
+spinner.succeed("Joined meeting")
 
-    // Message listener / scraper
-    await page.evaluate(_ => {
-        let observer = new MutationObserver((mutations) => {
-            mutations.forEach((mutation) => {
-                if (!mutation.addedNodes) return // If this is some other mutation
-                for (const node of mutation.addedNodes) {
-                    if (node.style.order === "0") return // If this is/these are the shell(s) that meet creates
-                    // Get msg info
-                    let info = {
-                        sender: node.childNodes[0].childNodes[0].innerHTML,
-                        time: node.childNodes[0].childNodes[1].innerHTML,
-                        text: node.childNodes[1].childNodes[0].innerHTML
-                    }
-                    window.message(info)
+spinner.start("Sending intro message")
+await page.click('[data-tooltip="Chat with everyone"]')
+await page.waitFor(1000) // 300 ms transition + 700 buffer
+await page.click('[name="chatTextInput"]')
 
-                    // We need a sub listener for
-                    // consecutive messages within the same minute by the same person
-                    // as they don't count as a new message object
+const shiftEnter = async _ => {
+	await page.keyboard.down("Shift")
+	await page.keyboard.press("Enter")
+	await page.keyboard.up("Shift")
+}
 
-                    let subObserver = new MutationObserver((mutations) => {
-                        mutations.forEach((mutation) => {
-                            if (!mutation.addedNodes) return // If this is some other mutation
-                            for (const node of mutation.addedNodes) {
-                                if (node.classList.contains("gYckH")) return // If this is/these are the shell(s) that meet creates
-                                // Get msg info
-                                let subInfo = {
-                                    sender: info.sender,
-                                    time: info.time,
-                                    text: node.innerHTML
-                                }
-                                window.message(subInfo)
-                            }
-                        })
-                    })
+await page.keyboard.type(
+	'Hello! I\'m Meetusic Bot, a bot that helps you play music from online sources. type "@meetusicbot /help" (without quotes) to see what I can do'
+)
+await shiftEnter()
+await page.keyboard.type("Version: 1")
+await shiftEnter()
+await page.keyboard.type("Developed by @khrj")
+await page.click('[data-tooltip="Send message"]')
+spinner.succeed("Send intro message")
 
-                    subObserver.observe(node.childNodes[1], { childList: true })
-                }
-            })
-        })
+// Get messages from window
+await page.exposeFunction("message", async msg => {
+	if (msg.text.includes("@meetusicbot") && msg.sender !== "You") {
+		msg.text = msg.text.replace("@meetusicbot", "")
 
-        let chatBox =  document.querySelector('[jsname="xySENc"]')
-        observer.observe(chatBox, { childList: true })
-    })
-    // await browser.close()
-})()
+		let parsed = {
+			command: false,
+			args: false,
+			error: false,
+		}
+
+		// Find command
+		for (const word of msg.text.trim().split(" ")) {
+			if (word.startsWith("/")) {
+				if (!parsed.command) {
+					parsed.command = word.replace("/", "")
+				} else {
+					parsed.error = "Too many commands"
+					parsed.command = false
+					parsed.args = false
+					break
+				}
+			}
+		}
+
+		if (!parsed.error) {
+			if (!parsed.command) {
+				parsed.error = "No commands"
+			} else {
+				parsed.args = msg.text.replace("/" + parsed.command, "").trim()
+			}
+		}
+
+		console.log(parsed)
+
+		// Continue logic here
+		if (parsed.error) {
+			await page.keyboard.type(`Error: ${parsed.error}`)
+			await page.click('[data-tooltip="Send message"]')
+		} else {
+			//TODO
+		}
+	}
+})
+
+// Message listener / scraper
+await page.evaluate(_ => {
+	let observer = new MutationObserver(mutations => {
+		mutations.forEach(mutation => {
+			if (!mutation.addedNodes) return // If this is some other mutation
+			for (const node of mutation.addedNodes) {
+				if (node.style.order === "0") return // If this is/these are the shell(s) that meet creates
+				// Get msg info
+				let info = {
+					sender: node.childNodes[0].childNodes[0].innerHTML,
+					time: node.childNodes[0].childNodes[1].innerHTML,
+					text: node.childNodes[1].childNodes[0].innerHTML,
+				}
+				window.message(info)
+
+				// We need a sub listener for
+				// consecutive messages within the same minute by the same person
+				// as they don't count as a new message object
+
+				let subObserver = new MutationObserver(mutations => {
+					mutations.forEach(mutation => {
+						if (!mutation.addedNodes) return // If this is some other mutation
+						for (const node of mutation.addedNodes) {
+							if (node.classList.contains("gYckH")) return // If this is/these are the shell(s) that meet creates
+							// Get msg info
+							let subInfo = {
+								sender: info.sender,
+								time: info.time,
+								text: node.innerHTML,
+							}
+							window.message(subInfo)
+						}
+					})
+				})
+
+				subObserver.observe(node.childNodes[1], { childList: true })
+			}
+		})
+	})
+
+	let chatBox = document.querySelector('[jsname="xySENc"]')
+	observer.observe(chatBox, { childList: true })
+})
+// await browser.close()
